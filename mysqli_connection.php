3d2a4ebb63e351a4d5396bc87b55ea06a7e08a3c
addisongernannt <addison.gernannt@gmail.com>
2017-03-25 22:52:48 -0400

    Create mysqli_connection.php

---
+++ mysqli_connection.php
@@ -0,0 +1,10 @@
+<?php
+// if you use a different port, please use that port number
+$con=mysqli_connect("168.16.222.102","addisongernannt","DoNtScr3wTh!$up!","addisongernannt1617db");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
